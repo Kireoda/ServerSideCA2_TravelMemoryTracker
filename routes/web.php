e3cc1b5e94777b4494c39010f7ea49b660b3ed24
Kireoda <anastasijabernsteine@gmail.com>
2026-03-31 12:37:00 +0100

    Implement core structure for Travel Memory Tracker
    Replaced cache and job-related migrations with migrations, models, controllers, and routes for `trips` and `memories`. Added basic views for trips and memories. Updated JetBrains project settings.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TripController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Trip routes
+Route::get('/trips', [TripController::class, 'index']);
+Route::get('/trips/create', [TripController::class, 'create']);
+Route::post('/trips', [TripController::class, 'store']);
+Route::get('/trips/{id}', [TripController::class, 'show']);
+Route::get('/trips/{id}/edit', [TripController::class, 'edit']);
+Route::put('/trips/{id}', [TripController::class, 'update']);
+Route::delete('/trips/{id}', [TripController::class, 'destroy']);
