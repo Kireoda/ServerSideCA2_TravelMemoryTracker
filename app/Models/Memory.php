3549bc27a1e2347caeddc1a1f7116eda8213faef
Kireoda <anastasijabernsteine@gmail.com>
2026-03-31 14:48:43 +0100

    Add trips and memories database structure

--- app/Models/Memory.php
+++ app/Models/Memory.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Memory extends Model
+{
+    protected $fillable = [
+        'trip_id',
+        'title',
+        'location',
+        'date',
+        'description'
+    ];
+
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+}
--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    protected $fillable = [
+        'title',
+        'location',
+        'start_date',
+        'end_date',
+        'description',
+        'user_id'
+    ];
+
+    public function memories()
+    {
+        return $this->hasMany(Memory::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
